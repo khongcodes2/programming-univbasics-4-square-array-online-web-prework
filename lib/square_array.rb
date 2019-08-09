60e65b5958d962a2bcd3a5ff9bed1bfcd633b4f0
K Hong <albtrossfacefall@gmail.com>
2019-08-09 03:30:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  
+  counter=0
+  squared_array=[]
+  while counter<array.length 
+    squared_array[counter]=array[counter]**2
+    counter += 1
+  end
+  squared_array
 end
